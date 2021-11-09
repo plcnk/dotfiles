da7f96a88c161245e406c8ac077285c5beefa229
plcnk <romain.pluciennik@gmail.com>
2021-11-09 15:21:42 +0100

    Add comments

--- packages.sh
+++ packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Install basic packages
 sudo apt install -y \
 vim \
 zsh \
@@ -7,10 +8,12 @@ htop \
 neofetch \
 git
 
+# Install Ansible
 sudo apt install software-properties-common -y
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
+# Install Terraform & Vault
 sudo apt install -y gnupg software-properties-common curl
 curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
