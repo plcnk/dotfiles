80ee82ad1ae3769b6d735acbe5ba8376e5888e26
plcnk <plcnk@protonmail.com>
2022-06-27 12:04:31 +0200

    Update packages & .zshrc

--- packages.sh
+++ packages.sh
@@ -11,17 +11,6 @@ neofetch \
 git \
 curl \
 software-properties-common \
-gnupg
-
-# Install Ansible (Debian uses the same source as Ubuntu)
-echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" > /etc/apt/sources.list.d/ansible.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
-apt update
-apt install ansible -y
-
-# Install Terraform & Vault
-curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
-apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-apt update
-apt install terraform -y
-apt install vault
+gnupg \
+python3-venv \
+python3-pip
